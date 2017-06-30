1fb93de7dc9fe25d5a2e25ec00389bd07e18421d
wzj <15051831028@163.com>
2017-06-30 19:05:17 +0800

    The third Edition on kernel.

--- kernel.py
+++ kernel.py
@@ -27,7 +27,7 @@ train_df = pd.read_csv('/Users/wzj/Documents/Study/Data Ming Project/kaggle_Tita
 test_df = pd.read_csv('/Users/wzj/Documents/Study/Data Ming Project/kaggle_Titanic Problem/Data/test.csv')
 combine = [train_df, test_df]
 
-# Step2:Get some basic info about Features
+# Step2:Get some basic info about Features (Objective)
 print(train_df.columns.values)
 print(train_df.head())
 
@@ -55,7 +55,6 @@ test_df.info()
      Cabin(77.1%) > Age(19.9%) > Embarked(<1%)
 '''
 print(train_df.describe())
-
 '''
   Analysis:
 1:Total samples are 891 or 40% of the actual number of passengers on board the Titanic (2,224).notice:2224 is given in the problem description.
@@ -77,11 +76,49 @@ print(train_df.describe(include=['O']))
 5:Ticket feature has high ratio (22%) of duplicate values (unique=681).
 '''
 
-#Step3: put some Assumtions based on the analysis
+#Step3: put some Assumtions based on the analysis (Subjective)
 
+'''
+Note :(1) 7c principle
+'''
 '''
 Assumtions:
-
-
-
+1 Correlating:(1)(major) Goal Solution : Feature Survived ; So, any other features correlating to Survived should take 
+                                         into account.
+                 Assumtion1:=#1  Pclass , Sex , Age , SibSp , Parch  (level 1 : most likely)
+                             #2  Embarked , Fare , Cabin , Ticket (level 2 : uncertain)
+                             #3  Name , PassengerId , Survived (level 3 : most unlikely)
+              (2)（Secondary）The link among other features : one feature may influence another feature , to make this 
+                              clear is a very complicated and challenging task !
+                             #4  Pclass->Name-Ticket-Fare-Carbin
+2 Completing : #5 Age , Embarked have NaN Value ,Once decision that they have correlation have been pulled out, they 
+                  should be completing.
+3 Correcting : #6 Since the feature Ticket have 681 unique value (total :891) as a nonnumerical type , Drop it off is a 
+                  wiser choice.
+               #7 Canbin have too many null value to consider it ?(or change the Carbin to bool type 'HaveCarbin')
+                  (argument Assumption)
+4 Creating :   #8 Since the number of person who have childern & parents & Sib &Sp is too small ,draw temp together, 
+                  Creating a new feature : FamilySize (total # of the family members)
+               #9 By ob the name ,we can get something like "Mr , Mis ... ", so we can creat a new feature called Title.
+               #10 continue type -> discrete type : Create a new "AgeBand" substitute feature "Age".
+               #11 Create a "FareBand" substitute "Fare" if the feature help our solution goals.
+5 Classifying :#12 Women (Sex=female) were more likely to have survived.
+               #13 Children (Age<?) were more likely to have survived.
+               #14 The upper-class passengers (Pclass=1) were more likely to have survived.
 '''
+
+#Step4: verifying Assumption & Deciding
+
+print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).size())
+print(train_df[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+'''
+Analyze by pivoting features
+(1) Pclass : approval #1
+(2) Sex : approval #1
+(3) SibSp & Parch 
+(4) Age: 
+'''
+
